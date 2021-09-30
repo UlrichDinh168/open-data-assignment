ee981bea4d059aa439313d6b10fc5808419b30c1
Ulrich <ulrich.dinh168@gmail.com>
2021-09-30 22:21:51 +0300

    add model and route for user

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, require: true },
   email: {
     type: String,
     required: true,
--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -6,87 +6,66 @@ import User from "../models/user";
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: "Mail exists",
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err,
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-            });
-            user
-              .save()
-              .then((result) => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created",
-                });
-              })
-              .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                  error: err,
-                });
-              });
-          }
-        });
-      }
+  const { name, email, password } = req.body;
+  try {
+    const existUser = User.find({ email });
+    if (existUser)
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await UserModal.create({
+      email,
+      password: hashedPassword,
+      name: ` ${name}`,
     });
+    const token = jwt.sign({ email: result.email, id: result._id }, secret, {
+      expiresIn: "1h",
+    });
+
+    result
+      .save()
+      .then((result) => {
+        console.log(result);
+        res.status(201).json({
+          message: "User created",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+    res.status(201).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(error);
+  }
 });
 
 router.post("/login", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth failed",
-        });
-      }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth failed",
-          });
-        }
-        if (result) {
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0]._id,
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h",
-            }
-          );
-          return res.status(200).json({
-            message: "Auth successful",
-            token: token,
-          });
-        }
-        res.status(401).json({
-          message: "Auth failed",
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
+  try {
+    const { email, password } = req.body;
+    const existUser = await UserModal.find({ email });
+    if (!existUser)
+      return res.status(404).json({ message: "User doesn't exist" });
+    const isCorrectPassword = await bcrypt.compare(
+      password,
+      existUser.password
+    );
+    if (!isCorrectPassword)
+      return res.status(400).json({ message: "Invalid credentials" });
+    const token = jwt.sign(
+      { email: existUser.email, id: existUser._id },
+      secret,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ result: existUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(error);
+  }
 });
 
 export default router;
