85d976803cb576de5d85a882505a358868233792
Ulrich <ulrich.dinh168@gmail.com>
2021-10-03 02:04:08 +0300

    add Auth Types

---
+++ client/src/actions/types.js
@@ -0,0 +1,19 @@
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const UPDATE_RECORDS = "UPDATE_RECORDS";
+export const DATE_PICKER_CHANGE = "DATE_PICKER_CHANGE";
+export const SLIDER_CHANGE = "SLIDER_CHANGE";
+export const STOP_RECORDS = "STOP_RECORDS";
+
+export const authTypes = {
+  login: "LOGIN",
+  loginSuccess: "LOGIN_SUCCESS",
+  loginFail: "LOGIN_FAIL",
+
+  signup: "SIGNUP",
+  signupSuccess: "SIGN_UP_SUCCESS",
+  signupFail: "SIGN_UP_FAIL",
+
+  logout: "LOGOUT",
+};
