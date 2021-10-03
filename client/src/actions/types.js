3a88e3ebbb8d55fe463a9212cdf208be3e26e0a7
Ulrich <ulrich.dinh168@gmail.com>
2021-10-03 18:08:25 +0300

    pre styling

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -17,3 +17,15 @@ export const authTypes = {
 
   logout: "LOGOUT",
 };
+
+export const sensorTypes = {
+  fetchAllSensors: "SENSORS_FETCH_ALL",
+  fetchAllSensorsSuccess: "SENSORS_FETCH_ALL_SUCCESS",
+  fetchAllSensorsFail: "SENSORS_FETCH_ALL_FAIL",
+};
+
+// notification
+export const notificationTypes = {
+  showNotification: "NOTIFICATION_SHOW_NOTIFICATION",
+  resetNotification: "NOTIFICATION_RESET_NOTIFICATION",
+};
