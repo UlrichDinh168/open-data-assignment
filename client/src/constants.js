ed4fd62a6f342670c7fbd979b5fc18f134683bb0
Ulrich <ulrich.dinh168@gmail.com>
2021-10-06 03:12:05 +0300

    .

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,7 +4,7 @@ export const ROUTER_PATH = {
   SIGNUP: "/user/signup",
 };
 
-export const BACKEND_BASE_URL = "http://localhost:5000";
+export const BACKEND_BASE_URL = "https://open-data-ulrich.herokuapp.com";
 export const PERSIST_KEY = "root";
 
 export const NOTIFICATION_DURATION = 3000;
